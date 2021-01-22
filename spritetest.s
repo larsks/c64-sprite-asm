cae880b392b79d7fd425db026d74836687222c67
Lars Kellogg-Stedman <lars@oddbit.com>
2021-01-22 13:10:46 -0500

    make delay loop tunable for longer than $ff iterations

--- spritetest.s
+++ spritetest.s
@@ -1,5 +1,8 @@
                 .import         arrow,arrow_color,CINT
-                .import         sprite0,color0,pos0x,pos0y,msbx
+                .import         sprite0,color0,pos0x,pos0y,msbx,spenable
+
+                .segment        "ZEROPAGE"
+delayval:       .word           0
 
                 .segment        "CODE"
 
@@ -21,7 +24,13 @@ main:           .scope
 
                 ldy             #0
 loop:           sty             pos0x
+
+                lda             #$0f            ; delay between sprite movements
+                sta             delayval        ; otherwise it zips by too quickly
+                lda             #$ff
+                sta             delayval+1
                 jsr             delay
+
                 iny
                 bne             end
                 lda             msbx
@@ -30,10 +39,33 @@ loop:           sty             pos0x
 end:            jmp             loop
                 .endscope
 
+; @name delay
+;
+; generic delay loop. load iteration count into delayval.
 delay:          .scope
-                ldx             #$ff
+                pha
+                txa
+                pha
+                tya
+                pha
+
+                ldx             delayval+1
+                ldy             delayval
+
+loopx:          txa
+                beq loopy
                 dex
-                beq             end
-                jmp             delay
-end:            rts
+                jmp loopx
+
+loopy:          tya
+                beq end
+                dey
+                jmp             loopx
+
+end:            pla
+                tay
+                pla
+                tax
+                pla
+                rts
                 .endscope
