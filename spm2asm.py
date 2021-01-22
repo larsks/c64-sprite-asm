b592bf0c587507ac57205870ee9c14e9b0cdfce3
Lars Kellogg-Stedman <lars@oddbit.com>
2021-01-22 10:44:40 -0500

    only put sprite definitions in GFXDATA segment
    Since sprite data needs to be aligned on 64 byte boundaries, we don't
    want to put anything but sprite data in GFXDATA, because this would
    result in holey allocation.

--- spm2asm.py
+++ spm2asm.py
@@ -4,10 +4,12 @@ import sys
 with (open(sys.argv[1]) if len(sys.argv) > 0 else sys.stdin) as fd:
     sprites = json.load(fd)
 
-print(f'{"":16}{".segment":16}"GFXDATA"')
+print('; DO NOT EDIT (this file was generated automatically)')
 names = []
 for sprite in sprites['sprites']:
     names.append(sprite['name'])
+    print(f'{"":16}{".segment":16}"GFXDATA"')
+    print(f'{"":16}{".align":16}$40')
     print(f'{sprite["name"]}:')
     for row in sprite['pixels']:
         rowi = iter(row)
@@ -16,6 +18,7 @@ for sprite in sprites['sprites']:
         print(f'{"":16}{".byte":16}', end='')
         print(','.join('%{}'.format(''.join(f'{x}' for x in chunk)) for chunk in chunks))
 
+    print(f'{"":16}{".segment":16}"DATA"')
     label = f'{sprite["name"]}_color'
     names.append(label)
     label = f'{label}:'
