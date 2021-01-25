d6149c3a807078fd01a72f255ed9758ab2fa8d12
Lars Kellogg-Stedman <lars@oddbit.com>
2021-01-25 11:45:39 -0500

    nicer output in mousetest

--- mousetest.s
+++ mousetest.s
@@ -1,4 +1,4 @@
-                .import         CHROUT
+                .import         CHROUT,PLOT,CINT
                 .import         joyport1,potx,poty
                 .import         println
                 .import         printhex
@@ -11,27 +11,38 @@
                 ; mouse manual:
                 ; https://www.commodore.ca/manuals/funet/cbm/manuals/1351-mouse.txt
                 ; via https://www.c64-wiki.com/wiki/BIT_$hhll#Example_1
-main:
-                lda             potx
+main:           jsr             CINT                    ; clear the screen
+
+loop:           ldx             #0                      ; set cursor position to top left
+                ldy             #0
+                jsr             PLOT
+                lda             potx                    ; print out potx value
                 jsr             printhex
                 lda             #' '
                 jsr             CHROUT
-                lda             poty
+                lda             poty                    ; print out poty value
                 jsr             printhex
                 jsr             newline
 
-                lda             #%00010000
+                lda             #%00010000              ; check if fire button is down
                 bit             joyport1
-                bne             main
+                bne             nofire
 
-                lda             #<msg_fire
+fire:           lda             #<msg_fire              ; print "FIRE!" message
                 sta             strbase
                 lda             #>msg_fire
                 sta             strbase+1
                 jsr             println
+                jmp             loop
 
-                jmp             main
+nofire:         lda             #<msg_nofire            ; erase "FIRE!" message
+                sta             strbase
+                lda             #>msg_nofire
+                sta             strbase+1
+                jsr             println
+                jmp             loop
 
                 .endscope
 
 msg_fire:       .asciiz         "FIRE!"
+msg_nofire:     .asciiz         "     "
