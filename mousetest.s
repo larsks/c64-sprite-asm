2873057bbbc6a8de75210e11ce90375644b9ab49
Lars Kellogg-Stedman <lars@oddbit.com>
2021-01-25 14:27:09 -0500

    add comments and macros to mousetest.s

--- mousetest.s
+++ mousetest.s
@@ -1,3 +1,6 @@
+; mouse manual:
+; https://www.commodore.ca/manuals/funet/cbm/manuals/1351-mouse.txt
+
                 .import         CHROUT,PLOT,CINT
                 .import         joyport1,potx,poty
                 .import         println
@@ -7,42 +10,86 @@
 
                 .segment        "CODE"
                 .scope
-                ;
-                ; mouse manual:
-                ; https://www.commodore.ca/manuals/funet/cbm/manuals/1351-mouse.txt
-                ; via https://www.c64-wiki.com/wiki/BIT_$hhll#Example_1
-main:           jsr             CINT                    ; clear the screen
 
-loop:           ldx             #0                      ; set cursor position to top left
-                ldy             #0
-                jsr             PLOT
-                lda             potx                    ; print out potx value
+; ======================================================================
+;
+                ; print the label, noise bit, and position from a pot register
+                ; at the current screen position
+                .macro          printpot label,addr
+                lda             #label
+                jsr             CHROUT
+                lda             #' '
+                jsr             CHROUT
+
+                lda             addr
+                and             #%00000001              ; mask out everything but noise bit
                 jsr             printhex
                 lda             #' '
                 jsr             CHROUT
-                lda             poty                    ; print out poty value
+
+                lda             addr                    ; print position
+                ror                                     ; discard noise bit
+                and             #%00111111
                 jsr             printhex
                 jsr             newline
+                .endmacro
 
-                lda             #%00010000              ; check if fire button is down
+                ; wrapper for println routine that takes care of setup
+                .macro          _println addr
+                lda             #<addr
+                sta             strbase
+                lda             #>addr
+                sta             strbase+1
+                jsr             println
+                .endmacro
+
+                ; move cursor to given screen position
+                .macro          setpos row,col
+                clc                                     ; request set position
+                ldx             #row
+                ldy             #col
+                jsr             PLOT
+                .endmacro
+
+; ======================================================================
+
+main:           jsr             CINT                    ; clear the screen
+
+loop:           setpos          0,0                     ; set cursor position to top left
+
+                printpot        'X',potx
+                printpot        'Y',poty
+
+checkleft:      setpos          2,0
+
+                lda             #%00010000              ; check if left button is down
                 bit             joyport1
-                bne             nofire
+                bne             leftup
 
-fire:           lda             #<msg_fire              ; print "FIRE!" message
-                sta             strbase
-                lda             #>msg_fire
-                sta             strbase+1
-                jsr             println
-                jmp             loop
+leftdown:       _println        msg_leftdown            ; print "LEFT!" message
+                jmp             checkright
 
-nofire:         lda             #<msg_nofire            ; erase "FIRE!" message
-                sta             strbase
-                lda             #>msg_nofire
-                sta             strbase+1
-                jsr             println
-                jmp             loop
+leftup:         _println        msg_leftup              ; erase "LEFT!" message
+
+checkright:     setpos          3,0
+                lda             #1
+                bit             joyport1
+                bne             rightup
+
+rightdown:      _println        msg_rightdown           ; print "RIGHT!" message
+                jmp             end
+
+rightup:        _println        msg_rightup             ; erase "RIGHT!" message
+
+end:            jmp loop
+
+; ======================================================================
 
                 .endscope
 
-msg_fire:       .asciiz         "FIRE!"
-msg_nofire:     .asciiz         "     "
+                .segment        "RODATA"
+
+msg_leftdown:   .asciiz         "LEFT!"
+msg_leftup:     .asciiz         "     "
+msg_rightdown:  .asciiz         "RIGHT!"
+msg_rightup:    .asciiz         "      "
